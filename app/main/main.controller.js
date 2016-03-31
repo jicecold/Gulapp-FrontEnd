83030e1c3edba64a3bbb6583d9220d7f969c235e
samuel <samuel.couto.25@gmail.com>
2016-03-30 22:14:10 -0300

    função de pesquisar

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -8,10 +8,18 @@
 	function ControllerMain(){
 		
 		var mainVm = this;
+		mainVm.search;
 		
 		mainVm.toggleSearchClick = function(){
-			
+		search.visible ? search.visible=false : search.visible=true; 
+		/*search.visible ? search.slideUp() : search.slideDown(function()
+		{
+			search.find('input').focus();
+		});*/
 			console.log("toggleSearchClick");
+
+			return false;
+			
 		}
 	}
 })();
