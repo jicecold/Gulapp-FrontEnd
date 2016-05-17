932ce11c38aef1f873c69799b7cb7cf0a7c63c21
Jair Batista Junior <jair.jr.batista@gmail.com>
2016-05-17 20:17:04 -0300

    Update main controller

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -1,25 +1,23 @@
 (function() {
 	'use strict';
-	
+
 	angular
 		.module('gulapp')
 		.controller('ControllerMain', ControllerMain);
-	
-	function ControllerMain(){
-		
+
+	function ControllerMain() {
+
 		var mainVm = this;
-		mainVm.search;
-		
-		mainVm.toggleSearchClick = function(){
-		search.visible ? search.visible=false : search.visible=true; 
-		/*search.visible ? search.slideUp() : search.slideDown(function()
-		{
-			search.find('input').focus();
-		});*/
+		mainVm.search = false;
+
+		mainVm.toggleSearchClick = function() {
+
+			mainVm.search = !mainVm.search;
+			
 			console.log("toggleSearchClick");
 
 			return false;
-			
+
 		}
 	}
 })();
